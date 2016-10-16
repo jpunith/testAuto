89a365ba731a988bf47c8001577840d5db6e9ae7
jpunith <p9019733334>
2016-10-16 20:45:20 +0530

    Adding image

--- app/scripts/directives/tea-input.js
+++ app/scripts/directives/tea-input.js
@@ -15,7 +15,7 @@ angular.module('testAutomationApp')
       '</select>'+
       '<select class="form-control" ng-options="boolean for boolean in booleanList" ng-if="teaInputType == \'boolean\'" ng-model="ngModel" ng-disabled="ngDisabled">' +
       '</select>'+
-      '<textarea class="form-control" ng-if="teaInputType == \'text:multiline\'" ng-model="ngModel" ng-disabled="ngDisabled">' +
+      '<textarea class="form-control" ng-if="teaInputType == \'text:multiline\'" ng-model="ngModel" ng-disabled="ngDisabled" rows="2">' +
       '</textarea>'+
       '<input class="form-control" ng-if="!teaInputList || teaInputList[0] == \'\' && teaInputType == \'number\'" type="text" ng-model="ngModel" ng-disabled="ngDisabled">'+
       '<select  class="form-control" ng-if="teaInputType == \'number\' && teaInputList && teaInputList[0] != \'\'" ng-options="item for item in teaInputList" ng-if="teaInputType == \'boolean\'" ng-model="ngModel" ng-disabled="ngDisabled">' +
