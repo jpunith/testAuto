59f4e374170e8730a1e7ca53bf531ac2093ff0a3
punith.jain <punith.jain@accentur.com>
2016-10-17 17:07:06 +0530

    Adding scrlls

--- app/scripts/services/menu-service.js
+++ app/scripts/services/menu-service.js
@@ -8,11 +8,11 @@
  * Service in the testAutomationApp.
  */
 angular.module('testAutomationApp')
-  .service('menuService', function menuService($http) {
-    // AngularJS will instantiate a singleton by calling "new" on this function
-    this.getMenus = function(){
-      return $http.get('json/menu.json').then(function(responce){
-        return responce.data;
-      });
-    }
-  });
+    .service('menuService', function menuService($http) {
+      // AngularJS will instantiate a singleton by calling "new" on this function
+      this.getMenus = function(){
+        return $http.get('json/menu.json').then(function(responce){
+          return responce.data;
+        });
+      }
+    });
